67af98adc67e2242bbaa6a73eda428f90e1c902f
delahermossa <delahermossa@gmail.com>
2022-05-23 19:07:14 +0200

    Cambios

---
+++ index.js
@@ -0,0 +1,5 @@
+function cambiaTextoModal(textoh2,textoh3){
+    document.getElementById("precioModal").innerHTML="Precio: "+textoh2+"€";
+    document.getElementById("tipoModal").innerHTML="Tipo: "+textoh3;
+
+}
